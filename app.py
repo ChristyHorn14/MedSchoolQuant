2a60f26fd464bae02916c081088d65803ff85142
Chris Hornung <31520790+ChristyHorn14@users.noreply.github.com>
2024-04-26 18:34:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -140,5 +140,4 @@ def update_graph(selected_year_label, additional_filter):
 # This is for Gunicorn compatibility
 server = app.server
 
-if __name__ == '__main__':
-    app.run_server(debug=True)
+
